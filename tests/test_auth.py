ad0b6916948ee21f9596f2731481cf40f1266f44
Cory Crowley <ccrowley96@gmail.com>
2025-08-21 14:12:33 -0400

    Auth timezone test fix

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import pytest
 
@@ -23,7 +23,7 @@ def mock_config():
 # Mock successful auth response
 @pytest.fixture
 def mock_success_response() -> UserSignInWithApiKeyResponse:
-    expires_at = (datetime.now() + timedelta(hours=1)).isoformat()
+    expires_at = (datetime.now(timezone.utc) + timedelta(hours=1)).isoformat()
     return {"id_token": "mock-bearer-token-123", "expires_at": expires_at}
 
 
