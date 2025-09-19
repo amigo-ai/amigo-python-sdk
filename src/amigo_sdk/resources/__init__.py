fe45553ec7c727264f027c2897f8b696905b2275
Euijae Kim <8512075+euijae@users.noreply.github.com>
2025-09-18 22:28:06 -0400

    Update sdk_client

---
+++ src/amigo_sdk/resources/__init__.py
@@ -0,0 +1,18 @@
+from .conversation import AsyncConversationResource, ConversationResource
+from .organization import AsyncOrganizationResource, OrganizationResource
+from .role import AsyncRoleResource, RoleResource
+from .service import AsyncServiceResource, ServiceResource
+from .user import AsyncUserResource, UserResource
+
+__all__ = [
+    "AsyncConversationResource",
+    "ConversationResource",
+    "AsyncOrganizationResource",
+    "OrganizationResource",
+    "AsyncRoleResource",
+    "RoleResource",
+    "AsyncServiceResource",
+    "ServiceResource",
+    "AsyncUserResource",
+    "UserResource",
+]
